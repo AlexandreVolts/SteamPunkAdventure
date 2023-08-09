cd1be9d14561801acbc1cf94f3df886f8d2c7936
AlexandreVolts <alexandre.cochet@epitech.eu>
2023-08-09 14:23:23 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,13 +4,14 @@ import Title from "./Title";
 interface LayoutProps
 {
   children: ReactNode;
+  hideTitle?: boolean;
 }
 export default function Layout(props: LayoutProps)
 {
   return (
     <div className="flex items-stretch min-h-screen bg-layer2 bg-fixed bg-no-repeat bg-contain bg-bottom bg-blue-500 font-steam-punk">
       <div className="w-full">
-        <Title text={document.title} />
+        {hideTitle ? <></> : <Title text={document.title} />}
         {props.children}
       </div>
     </div>
