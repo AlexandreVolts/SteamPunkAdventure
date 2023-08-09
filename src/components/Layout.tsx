89f1ba176923ccaf608f86fc06eeddec05cedd87
AlexandreVolts <alexandre.cochet@epitech.eu>
2023-08-09 14:28:11 +0200

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout(props: LayoutProps)
   return (
     <div className="flex items-stretch min-h-screen bg-layer2 bg-fixed bg-no-repeat bg-contain bg-bottom bg-blue-500 font-steam-punk">
       <div className="w-full">
-        {hideTitle ? <></> : <Title text={document.title} />}
+        {props.hideTitle ? <></> : <Title text={document.title} />}
         {props.children}
       </div>
     </div>
